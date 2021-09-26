447c0ae52166996241f7a5d8fde6ac3d920be238
root <root@m2-maprts-vm97-172.mip.storage.hpecorp.net>
2021-09-25 17:35:22 -0700

    The second file

---
+++ penultimate.py
@@ -0,0 +1,9 @@
+def bubsort(list_a):
+    indexing_length = len(list_a) -1
+    print (indexing_length)
+    x = indexing_length -1
+    print (list_a[x])
+    
+    #for i in range (0,indexing_length):
+    #    print (list_a[x])
+bubsort([10,11,12,14,18,19])
